2d9e78460865060c6fd691049b6f8a67fa2cf7ad
sohnryang <loop.infinitely@gmail.com>
2017-12-12 20:58:58 +0900

    added markdown rendering in post lists

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.db.models.query import QuerySet
 from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
@@ -12,6 +13,8 @@ import markdown
 # List of posts on homepage
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
+    for i in range(len(posts)):
+        posts[i].text = markdown.markdown(posts[i].text)
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 # List of unpublished posts (drafts)
